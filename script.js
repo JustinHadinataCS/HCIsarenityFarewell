449c9253d9010d65b704e35a1ab242f943274db6
Justin Hadinata <Justinhadinata.23@gmail.com>
2023-12-10 23:03:31 +0700

    loginpageJS Update

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+const signUpButton = document.querySelector('#signUpButton');
+const logInButton = document.querySelector('#logInButton');
+
+
+signUpButton.addEventListener('click', function () {
+    location.assign("http://127.0.0.1:5501/login.html");
+});
+
+logInButton.addEventListener('click', function () {
+    location.assign("http://127.0.0.1:5501/login.html");
+});
