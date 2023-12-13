2ef7708be6259a34719f1e03b8727d8b892d9ba4
Justin Hadinata <Justinhadinata.23@gmail.com>
2023-12-13 21:06:42 +0700

    toggle theme switcher

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ signUpButton.addEventListener('click', function () {
 logInButton.addEventListener('click', function () {
     location("https://justinhadinatacs.github.io/HCIsarenityFarewell/login.html");
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    let toggleButton = document.querySelector('.switch10 input');
+
+    toggleButton.addEventListener('click', function () {
+      let body = document.body;
+
+      if (body.classList.contains('dark')) {
+        body.classList.remove('dark');
+        body.classList.add('light');
+      } else {
+        body.classList.remove('light');
+        body.classList.add('dark');
+      }
+    });
+  });
