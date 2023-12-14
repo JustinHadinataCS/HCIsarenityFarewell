cbb379c52c10a312a2dcd64930b78d16488d76b9
Justin Hadinata <Justinhadinata.23@gmail.com>
2023-12-14 19:57:53 +0700

    Toggle - JSlogic Update

--- login.js
+++ login.js
@@ -1,17 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let loginButton = document.querySelector('.js-login');
-    let loginForm = document.querySelector('form');
-    let warningSign = document.querySelector('.js-reminder');
-
-    loginButton.addEventListener('click', function (event) {
-        if (!loginForm.checkValidity()) {
-            warningSign.innerHTML = 'Incorrect Password or Username'
-            loginButton.classList.add('animate__animated', 'animate__headShake');
-            event.preventDefault();
-        }
+document.addEventListener('DOMContentLoaded', function() {
+    // Update the JavaScript selector to match the new class name
+    var showLoginFormBtn = document.getElementById('showLoginFormBtn');
+    var mainContainer = document.querySelector('.main-container-100');
+  
+    // Add a click event listener to the button
+    showLoginFormBtn.addEventListener('click', function() {
+      // Toggle the display property of the main-container when the button is clicked
+      if (mainContainer.style.display === 'none' || mainContainer.style.display === '') {
+        mainContainer.style.display = 'block';
+      } else {
+        mainContainer.style.display = 'none';
+      }
     });
-
-    loginButton.addEventListener('animationend', function () {
-        loginButton.classList.remove('animate__animated', 'animate__headShake');
-    });
-});
+  });
+  
+  
