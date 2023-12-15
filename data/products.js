dc2f95494e3c0f4364b51588dde87ff20a9fb7c6
Justin Hadinata <Justinhadinata.23@gmail.com>
2023-12-15 12:17:18 +0700

    checkout UPDATE

--- data/products.js
+++ data/products.js
@@ -22,56 +22,56 @@ export const products =[
     id: "SerenityIDSMW3",
     image: "images/products/WoodCoffin2.jpg",
     name: "Italian Maple Coffin",
-    priceCents: 6990,
+    priceCents: 7600,
     keywords: ["coffin", "expensive", "apparel"]
   },
   {
     id: "SerenityIDRRI8",
     image: "images/products/AngelStone3.jpg",
     name: "An Angel Stone Memorial Grave",
-    priceCents: 6990,
+    priceCents: 5100,
     keywords: ["stone", "cheap", "apparel"]
   },
   {
     id: "SerenityIDR7FP",
     image: "images/products/WhiteMarble4.jpg",
     name: "Authentic White Marble Urn",
-    priceCents: 6990,
+    priceCents: 2800,
     keywords: ["stone", "cheap", "apparel"]
   },
   {
     id: "SerenityIDCOFH",
     image: "images/products/Urn5.jpg",
     name: "Italian Wooden 1980s Urn",
-    priceCents: 6990,
+    priceCents: 1700,
     keywords: ["stone", "cheap", "apparel"]
   },
   {
     id: "SerenityIDZRPL",
     image: "images/products/Urn6.jpg",
     name: "Silver Authentic Urn",
-    priceCents: 6990,
+    priceCents: 2500,
     keywords: ["stone", "cheap", "apparel"]
   },
   {
     id: "SerenityIDYWCR",
     image: "images/products/GoldenCasket7.jpg",
-    name: "Ancient Golden Casket",
-    priceCents: 6990,
+    name: "Egyptian Golden Dynasty Casket",
+    priceCents: 69900,
     keywords: ["stone", "cheap", "apparel"]
   },
   {
     id: "SerenityID8C9H",
     image: "images/products/Casket8.jpg",
-    name: "An Angel Stone Memorial Grave",
-    priceCents: 6990,
+    name: "Italian Woodcraft: Rusalka Casket Inspired by Russia's Elegance",
+    priceCents: 18900,
     keywords: ["stone", "cheap", "apparel"]
   },
   {
     id: "SerenityIDJU89",
     image: "images/products/Urn9.jpg",
-    name: "An Angel Stone Memorial Grave",
-    priceCents: 6990,
+    name: "Night Life Urn",
+    priceCents: 8600,
     keywords: ["stone", "cheap", "apparel"]
   },
   {
