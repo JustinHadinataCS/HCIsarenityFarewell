2acb7ec030b924c5c13615fce292d4cff3b5f745
Justin Hadinata <Justinhadinata.23@gmail.com>
2023-12-15 10:22:44 +0700

    x

--- data/products.js
+++ data/products.js
@@ -10,86 +10,75 @@ export function getProduct(productId) {
   return matchingProduct;
 }
 
-export const products = [
+export const products =[
   {
-    id: " serenityidzwo22",
+    id: "SerenityIDWTLZ",
     image: "images/products/GoldenCandle1.jpg",
     name: "Australian Opulence: Gilt-Edged Heritage Vintage Candle",
     priceCents: 6990,
-    keywords: [
-      "candle",
-      "expensive",
-      "apparel"
-    ]
-  },   {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    keywords: ["candle", "expensive", "apparel"]
+  },
+  {
+    id: "SerenityIDSMW3",
     image: "images/products/WoodCoffin2.jpg",
     name: "Italian Maple Coffin",
     priceCents: 6990,
-    keywords: [
-      "coffin",
-      "expensive",
-      "apparel"
-    ]
-  },  {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    keywords: ["coffin", "expensive", "apparel"]
+  },
+  {
+    id: "SerenityIDRRI8",
     image: "images/products/AngelStone3.jpg",
     name: "An Angel Stone Memorial Grave",
     priceCents: 6990,
-    keywords: [
-      "stone",
-      "cheap",
-      "apparel"
-    ]
-  }, {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    keywords: ["stone", "cheap", "apparel"]
+  },
+  {
+    id: "SerenityIDR7FP",
+    image: "images/products/WhiteMarble4.jpg",
+    name: "Authentic White Marble Urn",
+    priceCents: 6990,
+    keywords: ["stone", "cheap", "apparel"]
+  },
+  {
+    id: "SerenityIDCOFH",
+    image: "images/products/Urn5.jpg",
+    name: "Italian Wooden 1980s Urn",
+    priceCents: 6990,
+    keywords: ["stone", "cheap", "apparel"]
+  },
+  {
+    id: "SerenityIDZRPL",
+    image: "images/products/Urn6.jpg",
+    name: "Silver Authentic Urn",
+    priceCents: 6990,
+    keywords: ["stone", "cheap", "apparel"]
+  },
+  {
+    id: "SerenityIDYWCR",
+    image: "images/products/GoldenCasket7.jpg",
+    name: "Ancient Golden Casket",
+    priceCents: 6990,
+    keywords: ["stone", "cheap", "apparel"]
+  },
+  {
+    id: "SerenityID8C9H",
+    image: "images/products/Casket8.jpg",
+    name: "An Angel Stone Memorial Grave",
+    priceCents: 6990,
+    keywords: ["stone", "cheap", "apparel"]
+  },
+  {
+    id: "SerenityIDJU89",
+    image: "images/products/Urn9.jpg",
+    name: "An Angel Stone Memorial Grave",
+    priceCents: 6990,
+    keywords: ["stone", "cheap", "apparel"]
+  },
+  {
+    id: "SerenityIDGNJ4",
     image: "images/products/AngelStone3.jpg",
     name: "An Angel Stone Memorial Grave",
     priceCents: 6990,
-    keywords: [
-      "stone",
-      "cheap",
-      "apparel"
-    ]
-  }, {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/AngelStone3.jpg",
-    name: "An Angel Stone Memorial Grave",
-    priceCents: 6990,
-    keywords: [
-      "stone",
-      "cheap",
-      "apparel"
-    ]
-  }, {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/AngelStone3.jpg",
-    name: "An Angel Stone Memorial Grave",
-    priceCents: 6990,
-    keywords: [
-      "stone",
-      "cheap",
-      "apparel"
-    ]
-  }, {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/AngelStone3.jpg",
-    name: "An Angel Stone Memorial Grave",
-    priceCents: 6990,
-    keywords: [
-      "stone",
-      "cheap",
-      "apparel"
-    ]
-  }, {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/AngelStone3.jpg",
-    name: "An Angel Stone Memorial Grave",
-    priceCents: 6990,
-    keywords: [
-      "stone",
-      "cheap",
-      "apparel"
-    ]
+    keywords: ["stone", "cheap", "apparel"]
   }
 ];
